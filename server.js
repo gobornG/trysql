5206d659a8c501c4ff9b122f8a79dd6eb2efbb04
Garry <garry@Garrys-MacBook-Pro.local>
2017-10-02 17:26:30 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const massive = require('massive');
+const bodyParser = require('body-parser');
+
+const server = express();
+const port = 9000;
+var database;
+
+server.use(bodyParser.json());
+
+const pgConnectionConfig = 'postgres://dm:dm@localhost/dm';
+
+massive(pgConnectionConfig).then(dbInstance => {
+    database = dbInstance;
+    console.log('\n\n==[ Database Connected Successfully!! ]==\n\n');
+})
+
+//build endpoints here
+server.post('/users', (req, res) => {
+    //check if user has a name
+    //save the user
+    //return the user
+    if(req.body.name && req.body.name.length > 0) {
+        database.users.save(req.body).then(function(user) {
+            res.status(201).json(user);
+        });
+    } else {
+        res.status(400).json({ message: 'The user must have a name'});
+    }
+});
+
+    server.get('/users', (req, res) => {
+        database.users.find().then(function(users) {
+            res.status(200).send(users);
+        })
+    });
+
+    // server.get('users/:id', (req, res) => {
+    //     var userId = req.params.id;
+    //     database.users.findOne({
+    //         id: userId
+    //       }, {
+    //         columns: ['name', 'age']
+    //       }).then(result => {
+    //         res.status(200).json(user);
+    //       });
+
+
+server.listen(port, () => {
+    console.log(`server running on port ${port}`);
+});
